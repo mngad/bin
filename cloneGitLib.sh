c472fc1653d561dca00e11c1b631e42f0d3e9079
Gavin Day <mngad@leeds.ac.uk>
2017-07-25 15:24:46 +0200

    changed to use ssh instead of http

--- cloneGitLib.sh
+++ cloneGitLib.sh
@@ -1,2 +1,1 @@
-
-curl -sSL "https://api.github.com/users/mngad/repos?per_page=500" | grep 'clone_url' | cut -d '"' -f 4 | parallel git clone "$@"
+curl -sSL "https://api.github.com/users/mngad/repos?per_page=500" | grep 'ssh_url' | cut -d '"' -f 4 | parallel git clone "$@"
