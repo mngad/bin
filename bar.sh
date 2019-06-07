a5a537c90cd2f6e56fe014187f9b383b50a429df
fraun <mengda@leeds.ac.uk>
2019-06-07 09:53:27 +0100

    added brightness and bat

--- bar.sh
+++ bar.sh
@@ -1,6 +1,6 @@
 while true; do
 	BAT=$(cat /sys/devices/platform/smapi/BAT0/remaining_percent)
-	BACK=$(xbacklight | cut -c1-2)
+	BACK=$(xbacklight | cut -d'.' -f1)
 	xsetroot -name " BR: $BACK | BAT: $BAT% | $(date) "
 	sleep 2
 done
